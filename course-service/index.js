82293392195414841043fbf2bf7e1dfb460d55fc
huy-raw <ruacon130@gmail.com>
2022-06-17 21:11:26 +0700

    second commit

---
+++ course-service/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+
+//Constant definitions
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+//App 
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send({
+        "courses": [
+            "HTML", "CSS", "Docker", "OOP"
+        ]
+    })
+});
+
+
+app.listen(PORT, HOST);
+console.log("Running on  http://${HOST}:${PORT}");
